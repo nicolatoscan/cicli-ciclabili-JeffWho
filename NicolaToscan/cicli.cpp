bb0d1b21541cd091b39e12bdc7bc4a75908b0922
Nicola Toscan <nicolatoscan99@outlook.it>
2019-12-06 15:31:43 +0100

    non va mica troppo

--- NicolaToscan/cicli.cpp
+++ NicolaToscan/cicli.cpp
@@ -5,19 +5,25 @@
 #include <utility>
 #include <queue>
 #include <stack>
+#include <iomanip>
 
 using namespace std;
 
 void findArticulations(vector<int> *g, int n, bool *visited, int *disc, int *low, int *parent);
 void findArticulationsWrapper(vector<int> *g);
 void criccaFinder(vector<int> *g, vector<int> *cricca, vector<int> *criccaToBackbone);
-void backBoneGraph(vector<int> *g);
-void DJ(vector<int> *g, int start);
+void backBoneGraph(vector<int> *g, vector<int> *backbone);
+void DJ(vector<int> *g, int start, int **distanceTo, bool log);
 
 bool isBackBone[10000];
+int nBacknbone = 0;
+int fromBackboneToN[10000];
+int fromNToBackbone[10000];
 
 int N, M, NREQ;
 int nCricche = 0;
+
+const int MAX_LENGTH = 100000;
 
 int main()
 {
@@ -28,6 +34,7 @@ int main()
     in >> NREQ;
 
     vector<int> *g = new vector<int>[N];
+    vector<int> *backbone = new vector<int>[N];
     vector<int> *cricca = new vector<int>[N];
     vector<int> *criccaToBackbone = new vector<int>[N];
 
@@ -42,56 +49,59 @@ int main()
 
     findArticulationsWrapper(g);
     criccaFinder(g, cricca, criccaToBackbone);
-    backBoneGraph(g);
+    backBoneGraph(g, backbone);
 
-    cout << "BACKBONE" << endl;
-    for (int i = 0; i < N; i++)
-        if (isBackBone[i])
-            cout << i << " ";
-    cout << endl
-         << endl;
-
-    cout << "CRICCHE" << endl;
-    for (int i = 0; i < nCricche; i++)
+    if (true)
     {
-        cout << i << "\t=> ";
-        for (int n : cricca[i])
-            cout << n << " ";
-        cout << endl;
-    }
-    cout << endl;
+        cout << "BACKBONE" << endl;
+        for (int i = 0; i < N; i++)
+            if (isBackBone[i])
+                cout << i << " ";
+        cout << endl
+             << endl;
 
-    cout << "CRICCA TO BACKBONE" << endl;
-    for (int i = 0; i < nCricche; i++)
-    {
-        cout << i << "\t=> ";
-        for (int n : criccaToBackbone[i])
-            cout << n << " ";
-        cout << endl;
-    }
-    cout << endl;
-
-    cout << "BACKBONE GRAPH" << endl;
-    for (int i = 0; i < N; i++)
-    {
-        if (isBackBone[i])
+        cout << "CRICCHE" << endl;
+        for (int i = 0; i < nCricche; i++)
         {
-            cout << i << "\t=> ";
-            for (int n : g[i])
+            cout << i << " => ";
+            for (int n : cricca[i])
+                cout << n << " ";
+            cout << "\t\tB: ";
+            for (int n : criccaToBackbone[i])
                 cout << n << " ";
             cout << endl;
         }
-    }
-    cout << endl;
+        cout << endl;
 
-    for (int i = 0; i < N; i++)
-    {
-        if (isBackBone[i])
+        cout << "BACKBONE GRAPH" << endl;
+        for (int i = 0; i < nBacknbone; i++)
         {
-            cout << "Distance from " << i << endl;
-            DJ(g, i);
+            cout << fromBackboneToN[i] << "\t=> ";
+            for (int n : backbone[i])
+                cout << fromBackboneToN[n] << " ";
+            cout << endl;
         }
+        cout << endl;
     }
+
+    int **distanceTo = new int *[nBacknbone];
+    for (int i = 0; i < nBacknbone; i++)
+    {
+        distanceTo[i] = new int[nBacknbone];
+        for (int j = 0; j < nBacknbone; j++)
+            distanceTo[i][j] = i == j ? 0 : MAX_LENGTH;
+    }
+
+    for (int i = 0; i < nBacknbone; i++)
+        DJ(backbone, i, distanceTo, false);
+
+    for (int i = 0; i < nBacknbone; i++)
+    {
+        for (int j = 0; j < nBacknbone; j++)
+            cout << distanceTo[i][j] << " ";
+        cout << endl;
+    }
+
     return 0;
 }
 
@@ -176,68 +186,87 @@ void criccaFinder(vector<int> *g, vector<int> *cricca, vector<int> *criccaToBack
     }
 }
 
-void backBoneGraph(vector<int> *g)
+void backBoneGraph(vector<int> *g, vector<int> *backbone)
 {
+
     for (int i = 0; i < N; i++)
     {
-        if (!isBackBone[i])
+        if (isBackBone[i])
         {
-            g[i].clear();
-        }
-        else
-        {
-            vector<int> toReplace;
-            for (int n : g[i])
-                if (isBackBone[n])
-                    toReplace.push_back(n);
-            g[i] = toReplace;
+            fromBackboneToN[nBacknbone] = i;
+            fromNToBackbone[i] = nBacknbone;
+            nBacknbone++;
         }
     }
+    for (int i = 0; i < nBacknbone; i++)
+        for (int n : g[fromBackboneToN[i]])
+            if (isBackBone[n])
+                backbone[i].push_back(fromNToBackbone[n]);
 }
 
 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-int MAX_LENGTH = 100000;
-void DJ(vector<int> *g, int start)
+void DJ(vector<int> *g, int start, int **distanceTo, bool log)
 {
-    bool *visited = new bool[N];
-    int *distanceTo = new int[N];
-    int *pred = new int[N];
-    for (int i = 0; i < N; i++)
-    {
+    bool *visited = new bool[nBacknbone];
+    for (int i = 0; i < nBacknbone; i++)
         visited[i] = false;
-        distanceTo[i] = MAX_LENGTH;
-        pred[i] = -1;
-    }
-    distanceTo[start] = 0;
+
     q.push(make_pair(0, start));
 
+    int i = 0;
     while (!q.empty())
     {
+        int nextNode;
         pair<int, int> next = q.top();
         q.pop();
+        nextNode = next.second;
 
-        if (next.first >= MAX_LENGTH)
-            break;
+        if (log)
+            cout << "Sto facendo " << fromBackboneToN[nextNode] << endl;
 
-        if (!visited[next.second])
+        if (!visited[nextNode])
         {
-            visited[next.second] = true;
-            for (int n : g[next.second])
+            visited[nextNode] = true;
+            for (int n : g[nextNode])
             {
-                int alt = 1 + distanceTo[next.second];
+                if (log)
+                    cout << "Lavoro tra " << fromBackboneToN[nextNode] << " e " << fromBackboneToN[n] << endl;
 
-                if (alt < distanceTo[n])
+                int alt = 1 + distanceTo[start][nextNode];
+                if (log)
+                    cout << fromBackboneToN[nextNode] << " è a " << distanceTo[start][nextNode] << endl;
+                if (log)
+                    cout << fromBackboneToN[n] << " è a " << distanceTo[start][n] << endl;
+
+                if (alt < distanceTo[start][n])
                 {
-                    pred[n] = next.second;
-                    distanceTo[n] = alt;
+                    distanceTo[start][n] = alt;
+                    distanceTo[n][start] = alt;
+                    if (log)
+                        cout << "Pushing " << fromBackboneToN[n] << " with " << alt << endl;
+
                     q.push(make_pair(alt, n));
                 }
+                if (log)
+                    cout << endl;
             }
         }
+        if (log)
+            cout << endl;
     }
+    if (log)
+    {
 
-    for (int i = 0; i < N; i++)
-        if (isBackBone[i])
-            cout << i << ":" << distanceTo[i] << "\t";
-    cout << endl;
+        cout << endl
+             << "Distance from " << fromBackboneToN[start] << endl;
+        for (int i = 0; i < nBacknbone; i++)
+            cout << fromBackboneToN[i] << ":" << distanceTo[start][i] << "   ";
+        cout << endl;
+        for (int i = 0; i < nBacknbone; i++)
+            cout << fromBackboneToN[i] << ":" << (visited[i] ? "SI" : "NO") << "   ";
+
+        cout << endl
+             << "-----------------------" << endl
+             << endl;
+    }
 }

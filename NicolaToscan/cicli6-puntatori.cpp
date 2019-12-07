2489110f5aa69f6218c20053763557a4ccb6426a
Nicola Toscan <nicolatoscan99@outlook.it>
2019-12-07 23:21:19 +0100

    fatta

--- NicolaToscan/cicli6-puntatori.cpp
+++ NicolaToscan/cicli6-puntatori.cpp
@@ -9,17 +9,33 @@
 
 using namespace std;
 
+struct UpTree {
+   UpTree* su;
+    int data;
+};
+
+void treeinator(vector<int> *g, int root);
+int findDistance(vector<int> *g, int p1, int p2);
+
 int N, M, NREQ;
-int depth[50000];
-int parent[50000];
-bool visited[50000];
+int nCricche = 0, nBacknbone = 0;
+int *depth;
+int *parent;
+bool *visited;
 
 queue<int> q;
 
-void treeinator(vector<int> *g, int root)
+void treeinator(vector<int> *g, int root, UpTree* tree)
 {
+    visited = new bool[N];
+    depth = new int[N];
+    parent = new int[N];
     for (int i = 0; i < N; i++)
+    {
         visited[i] = false;
+        depth[i] = -1;
+        parent[i] = -1;
+    }
 
     visited[root] = true;
     depth[root] = 0;
@@ -39,18 +55,23 @@ void treeinator(vector<int> *g, int root)
                 visited[n] = true;
                 depth[n] = depth[node] + 1;
                 parent[n] = node;
+
+                tree[n].data = n;
+                tree[n].su = &tree[node];
+
                 q.push(n);
             }
         }
     }
 }
 
-int findDistance2(vector<int> *g, int p1, int p2)
+int findDistance(vector<int> *g, int p1, int p2)
 {
     int dist = 0;
 
     int lastP1 = -1;
     int lastP2 = -2;
+    return 9;
 
     while (p1 != p2)
     {
@@ -84,42 +105,6 @@ int findDistance2(vector<int> *g, int p1, int p2)
     return dist;
 }
 
-int findDistance(vector<int> *g, int p1, int p2)
-{
-    int d1 = depth[p1];
-    int d2 = depth[p2];
-    if (d1 < d2)
-    {
-        int t = p1;
-        p1 = p2;
-        p2 = t;
-
-        t = d1;
-        d1 = d2;
-        d2 = t;
-    }
-
-    int dist = d1 - d2;
-    for (int i = 0; i < dist; i++)
-        p1 = parent[p1];
-
-    if (p1 == p2)
-        return dist;
-
-    while (parent[p1] != parent[p2])
-    {
-        dist += 2;
-        p1 = parent[p1];
-        p2 = parent[p2];
-    }
-
-    for (int n : g[p1])
-        if (n == p2 && depth[n] == depth[p1])
-            return dist + 1;
-
-    return dist + 2;
-}
-
 int main()
 {
     clock_t start, end;
@@ -142,6 +127,9 @@ int main()
         g[s].push_back(e);
         g[e].push_back(s);
     }
+
+    cout << g[2].size() << endl;
+
     treeinator(g, 0);
 
     int *i1 = new int[NREQ];
@@ -158,8 +146,8 @@ int main()
     end = clock();
     ofstream out("output.txt");
 
-    for (int i = 0; i < NREQ; i++)
-        out << i1[i] << "\n";
+    //for (int i = 0; i < NREQ; i++)
+    //    out << i1[i] << "\n";
 
     double res = double(end - start) / double(CLOCKS_PER_SEC);
     cout << fixed << res << setprecision(7) << endl;
